2d3afcb66600235d0588a46d68aa56cf7f86ccbc
Rakibul-Codex-BD <email_for_account1@example.com>
2025-02-04 19:54:21 +0600

    char basic

---
+++ class.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char name[]{"Ahsan"};
+    cout << name << endl;
+
+    char company[] = "Coding Me";
+    cout << company << endl;
+
+    char greeting[100];
+    cin >> greeting;
+    cout << greeting << endl;
+
+    return 0;
+}
